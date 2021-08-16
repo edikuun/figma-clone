f6d2bfae98bcbb3b762c28af8c601421614dbd76
Edison Llorada <edisonllorada@gmail.com>
2021-08-16 21:13:39 +0800

    Refactor main canvas

--- src/component/main/sections/main-canvas/index.tsx
+++ src/component/main/sections/main-canvas/index.tsx
@@ -1,8 +1,9 @@
-import { Fragment } from 'react';
 import { IComponent } from '../../index';
 import { Card, Row, Col, OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 import { DEFAULT_CARD_IMAGE } from '../../constants';
+
+import './index.scss';
 
 const MainCanvas = ({
   componentList,
@@ -10,39 +11,41 @@ const MainCanvas = ({
 }: {
   componentList: IComponent[];
   onClickComponent: (event: any, componentName: string) => void;
-}) => (
-  <Fragment>
-    <div className="mb-4 fw-bold">Main Canvas</div>
-    <Row className="mx-auto">
-      {componentList.map(({ name, description, properties }, index) => (
-        <Col className="mb-5">
-          <OverlayTrigger
-            placement="bottom"
-            delay={{ show: 250, hide: 400 }}
-            overlay={
-              <Tooltip id="button-tooltip">
-                Height: {properties?.height} Width: {properties?.width}
-              </Tooltip>
-            }
+}) => {
+  const compList = componentList.map(
+    ({ name, description, properties }: IComponent, index) => (
+      <Col className="mb-5">
+        <OverlayTrigger
+          placement="bottom"
+          delay={{ show: 250, hide: 400 }}
+          overlay={
+            <Tooltip id="button-tooltip">
+              Height: {properties?.height} Width: {properties?.width}
+            </Tooltip>
+          }
+        >
+          <Card
+            className="Card"
+            id={name}
+            key={index}
+            onClick={(event) => onClickComponent(event, name)}
           >
-            <Card
-              className="Card"
-              id={name}
-              key={index}
-              onClick={(event) => onClickComponent(event, name)}
-              style={{ width: '18rem' }}
-            >
-              <Card.Img variant="top" src={DEFAULT_CARD_IMAGE} />
-              <Card.Body>
-                <Card.Title>{name}</Card.Title>
-                <Card.Text>{description}</Card.Text>
-              </Card.Body>
-            </Card>
-          </OverlayTrigger>
-        </Col>
-      ))}
-    </Row>
-  </Fragment>
-);
+            <Card.Img variant="top" src={DEFAULT_CARD_IMAGE} />
+            <Card.Body>
+              <Card.Title>{name}</Card.Title>
+              <Card.Text>{description}</Card.Text>
+            </Card.Body>
+          </Card>
+        </OverlayTrigger>
+      </Col>
+    )
+  );
+  return (
+    <>
+      <div className="mb-4 fw-bold">Main Canvas</div>
+      <Row className="mx-auto">{compList}</Row>
+    </>
+  );
+};
 
 export default MainCanvas;
